592bb5e6df8375d441034dc8a7b0f3849a749ef3
biradarpooja <89473036+biradarpooja@users.noreply.github.com>
2021-08-26 17:42:39 +0530

    Asciivalue
    Java Program to find Ascii Value of character

---
+++ Asciivalue.java
@@ -0,0 +1,29 @@
+/*output*/ 
+/**The ASCII value of a is: 97
+*The ASCII value of b is: 98
+*/
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author biradar bharat
+ */
+public class Asciivalue {
+   public static void main(String[] args)   
+{  
+// character whose ASCII value to be found  
+char ch1 = 'a';  
+char ch2 = 'b';  
+// variable that stores the integer value of the character  
+int asciivalue1 = ch1;  
+int asciivalue2 = ch2;  
+System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+}   
+}
